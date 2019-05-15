d87868946f36bb24fe87da1954db8fe937321208
Isaac German <isaacgerman95@gmail.com>
2019-05-14 20:54:20 -0600

    created a number of new schemas, closes #3

---
+++ src/components/StartMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class StartMenu extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+    render(){
+        return (
+            <h1>Start Menu</h1>
+        )
+    }
+}
