bbe09582e8489940efcc8dfda21f2b4938549474
Mike Roberts <mikebroberts@hotmail.com>
2019-05-16 06:27:04 -0600

    created worldMap PromptTextbox and PlayerStatBox components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PlayerStatBox from './components/PlayerStatBox.js';
+import PromptTextBox from './components/PromptTextbox.js';
+import WorldMap from './components/WorldMap.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WorldMap/>
+      <PromptTextBox />
+      <PlayerStatBox />
     </div>
   );
 }
